31303537b0e7490db9774c12dfeaeb2c241140ab
Debojit219 <debojit.banerjee546@gmail.com>
2021-10-05 23:25:11 +0530

    Adding the landing Page as a branch to main(shrink animation to be updated later)

---
+++ public/scripts/landingPageScript.js
@@ -0,0 +1,17 @@
+// Demo or didn't happen
+// var signin = document.querySelector('#signin')
+// var register = document.querySelector('#register')
+// setTimeout(function(){ register.checked = true}, 1000)
+// setTimeout(function(){ signin.checked = true}, 2000)
+
+// $(document).ready(function(){
+//     $("#signinTab").click(function(){
+//         console.log("clicked!")
+//         $(".loginArea").addClass("shrink").removeClass("loginArea");
+//     })
+//     $("#registerTab").click(function(){
+//         console.log("clicked!")
+//         $(".shrink").addClass("loginArea");
+//         $(".loginArea").addClass("loginArea");
+//     })
+// })
